e128e03a3c209139833b3cc7378057f1f1a531fc
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-13 10:11:36 +1300

    Create MGSChildcare.py

---
+++ MGSChildcare.py
@@ -0,0 +1,7 @@
+"""You are to design and write a program that will be used by a child day-care
+centre. It will keep track of children throughout the day. There will be many
+features in the program, but we will take it a step at a time and build each
+one in turn.
+"""
+
+
