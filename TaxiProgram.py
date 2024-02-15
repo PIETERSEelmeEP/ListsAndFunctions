61869eee44fa55cfc269eb9479234f4ce8dd4481
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-16 11:09:29 +1300

    Create TaxiProgram.py

---
+++ TaxiProgram.py
@@ -0,0 +1,7 @@
+"""Write a program which keep track of details for a taxi. The program should
+start off by asking for the driver’s name. It should then repeatedly ask to
+start a new trip. If the user says "yes", ask for the time the trip took in
+minutes. It should print out the cost at the end of the trip which is a base
+cost of $10 plus $2 extra for each minute the trip took.
+"""
+
