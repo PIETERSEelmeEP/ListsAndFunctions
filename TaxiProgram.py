8cccf47320dedd0d5814aac1b35e2cbac38891cb
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-16 11:43:58 +1300

    Update TaxiProgram.py

--- TaxiProgram.py
+++ TaxiProgram.py
@@ -5,3 +5,63 @@ minutes. It should print out the cost at the end of the trip which is a base
 cost of $10 plus $2 extra for each minute the trip took.
 """
 
+
+def number_checker(question):
+    error = "\nPlease enter a name\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
+
+
+def answer_checker(question):
+    error = "\nPlease enter a name\n"
+    answer = ""
+    while not answer:
+        try:
+            answer = str(input(question))
+            return answer
+        except ValueError:
+            print(error)
+
+
+def yes_no_checker(question):
+    error = "\nPlease enter a valid answer (Yes or No)"
+    answer = ""
+    while not answer:
+        try:
+            answer = str(input(question)).lower()
+            if answer == "yes" or answer == "no":
+                return answer
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+def new_trip():
+    total_time = 0
+    while True:
+        start_new_trip = answer_checker("Do you want to start a new "
+                                        "trip? ").lower()
+        if start_new_trip == "yes":
+            FIXED_COST = 10
+            time_minutes = number_checker("Enter the duration of the trip -- "
+                                          "in minutes: ")
+            COST_PER_MINUTE = 2
+            final_cost = time_minutes * COST_PER_MINUTE + FIXED_COST
+            print(f"The trip cost ${final_cost}\n")
+            total_time += time_minutes
+        if start_new_trip == "no":
+            print(f"Name of driver is: {driver_name}")
+            print(f"Total time of all the trips is {total_time} minutes")
+            return
+
+
+# Main Routine
+driver_name = answer_checker("Enter drivers name: ")
+print()
+new_trip()
