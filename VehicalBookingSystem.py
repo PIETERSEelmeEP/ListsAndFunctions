822702bfc6d8706ab548217b09f9cde7fe1a1a59
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-20 19:09:45 +1300

    Update VehicalBookingSystem.py

--- VehicalBookingSystem.py
+++ VehicalBookingSystem.py
@@ -5,7 +5,23 @@ the day, and keeps track of which vehicles have already been booked.
 
 
 def availability():
+    global booked_by
     return booked_by is None
+
+
+def book(name):
+    global booked_by
+    booked_by = name
+
+
+def display_available_vehicles(seats_needed):
+    print("VEHICLE NUMBER - TYPE - NO. SEATS")
+    for vehicle in VEHICLE_LIST:
+        if vehicle[2] < seats_needed:
+            print(f"No. {vehicle[0]} -- {vehicle[1]} -- {vehicle[2]} seats -- "
+                  f"Note: Don't have enough seats")
+        else:
+            print(f"No. {vehicle[0]} -- {vehicle[1]} -- {vehicle[2]} seats")
 
 
 # Main Routine
@@ -14,9 +30,9 @@ VEHICLE_LIST = [
     (2, 'Toyota Corolla', 4),
     (3, 'Honda CRV', 4),
     (4, 'Suzuki Swift', 4),
-    (5, 'Mitsubishi Airtrek', 4),
+    (5, 'Mitsubishi Air-trek', 4),
     (6, 'Nissan DC Ute', 4),
-    (7, 'Toyota Previa', 7),
+    (7, 'Toyota Pre-via', 7),
     (8, 'Toyota Hi Ace', 12),
     (9, 'Toyota Hi Ace', 12)
 ]
@@ -24,6 +40,6 @@ number = 0
 car_type = ""
 seats = 0
 booked_by = None
-for vehicle in VEHICLE_LIST:
-    print(f"{vehicle[0]} -- {vehicle[1]} -- {vehicle[2]}")
-
+number_seats = int(input("Please enter the number of seats required (Type -1 "
+                         "to quit): "))
+display_available_vehicles(number_seats)
