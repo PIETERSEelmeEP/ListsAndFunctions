8e1cc0ab4a97431ffe6434b9d93e7d5fd02b4f12
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-20 18:38:06 +1300

    Create VehicalBookingSystem.py

---
+++ VehicalBookingSystem.py
@@ -0,0 +1,11 @@
+"""You will be designing basic software for a booking system for the local
+University's vehicle booking system. It allows staff to borrow a vehicle for
+the day, and keeps track of which vehicles have already been booked.
+"""
+
+
+# Main Routine
+print("Enter vehicle details:")
+number = input("Vehicle number: ")
+car_type = input("Car type: ")
+seats = input("Number of seats: ")
