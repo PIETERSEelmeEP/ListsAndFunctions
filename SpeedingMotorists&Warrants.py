a291ef7bbe63e4b6b187962696e307f7321ac1c1
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-20 17:59:01 +1300

    Create SpeedingMotorists&Warrants.py

---
+++ SpeedingMotorists&Warrants.py
@@ -0,0 +1,10 @@
+"""You will be designing basic software for a computer inside a police patrol
+car. The program keeps track of speeding tickets, and alerts the officer if
+an outstanding warrant to arrest the speeder.
+"""
+
+
+# Main Routine
+speeders = []
+wanted_list = {"James Wilson", "Helga Norman", "Zachary Conroy"}
+total_fines = 0
