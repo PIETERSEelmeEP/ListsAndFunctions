23feb129e2788fd8347fa32cec8b360d30a62fbd
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-20 18:04:02 +1300

    Update SpeedingMotorists&Warrants.py

--- SpeedingMotorists&Warrants.py
+++ SpeedingMotorists&Warrants.py
@@ -4,6 +4,31 @@ an outstanding warrant to arrest the speeder.
 """
 
 
+def issue_ticket(name, speed):
+    fine = calculate_fine(speed)
+
+
+def calculate_fine(speed):
+    if speed < 10:
+        return 30
+    elif 10 <= speed < 15:
+        return 80
+    elif 15 <= speed < 20:
+        return 120
+    elif 20 <= speed < 25:
+        return 170
+    elif 25 <= speed < 30:
+        return 230
+    elif 30 <= speed < 35:
+        return 300
+    elif 35 <= speed < 40:
+        return 400
+    elif 40 <= speed < 45:
+        return 510
+    else:
+        return 630
+
+
 # Main Routine
 speeders = []
 wanted_list = {"James Wilson", "Helga Norman", "Zachary Conroy"}
